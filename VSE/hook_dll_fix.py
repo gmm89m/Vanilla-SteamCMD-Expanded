4ccb46880dcbc634f21b3372929648e93ee403e5
gmm89m <150973951+gmm89m@users.noreply.github.com>
2026-04-02 15:41:30 +0300

    Delete VSE directory

--- VSE/hook_dll_fix.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-if getattr(sys, "frozen", False):
-    # Add _internal dir to DLL search path before anything else loads
-    _internal = os.path.join(os.path.dirname(sys.executable), "_internal")
-    os.environ["PATH"] = _internal + os.pathsep + os.environ.get("PATH", "")
-    if hasattr(os, "add_dll_directory"):
-        os.add_dll_directory(_internal)
